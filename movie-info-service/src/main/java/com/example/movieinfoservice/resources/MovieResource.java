6ade95d130e8ea8a8f725365cf55f13f9b7dcdda
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-24 17:02:29 +0530

    feat: Added logger

--- movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
@@ -1,5 +1,7 @@
 package com.example.movieinfoservice.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,12 @@ import com.example.movieinfoservice.model.Movie;
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MovieResource.class);
 
 	@RequestMapping("/{movieId}")
 	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+		logger.info("GETTING MOVIE INFO");
 		return new Movie(movieId, "Test movie");
 	}
 }
